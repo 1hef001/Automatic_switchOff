231b4083d270ae75c26c615deef35762ca3e6f04
S Ashwin <47474911+TheSinOfSloth@users.noreply.github.com>
2020-01-09 18:41:53 +0530

    Update idle.sh

--- idle.sh
+++ idle.sh
@@ -3,7 +3,7 @@
 # actually assign the variable
 idle_time=0
 # adapted if clause to actually match (see https://stackoverflow.com/questions/18668556/comparing-numbers-in-bash) 
-while [ "$idle_time" -lt "6000" ]; do
+while [ "$idle_time" -lt "900000" ]; do
     idle_time=$(idle_time=exec sudo -u "$whoami" xprintidle)
     echo "$idle_time"
 done
